f373b843aed54e23eefa6ef5ef72772dd4912fb5
오지환 <ovitwisdom@gmail.com>
2025-09-04 08:18:42 +0900

    dsa

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,6 @@
 // app/(tabs)/_layout.tsx
 import { Tabs } from "expo-router";
 
-if (typeof global.setImmediate === 'undefined') {
-    // @ts-ignore
-    global.setImmediate = (fn: (...a:any[]) => void, ...a:any[]) => setTimeout(fn, 0, ...a);
-}
-
 export default function TabsLayout() {
     return (
         <Tabs screenOptions={{ headerTitleAlign: "center" }}>
